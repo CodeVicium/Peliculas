dad9acb23a33983c4dc4bd72e0a93c63558329ce
CodeVicium <46329897+CodeVicium@users.noreply.github.com>
2019-04-10 11:52:05 -0300

    clase equipo
    son todos unos putoss;;;;

---
+++ objetosJava-master/objlab3/src/objlab3/Punto.java
@@ -0,0 +1,27 @@
+package objlab3;
+public class Punto {
+    private float y;
+    private float x;
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public Punto(float y, float x) {
+        this.y = y;
+        this.x = x;
+    }
+ 
+}
