c385bc591ca8c77bc3a94876c24977892437347a
CodeVicium <46329897+CodeVicium@users.noreply.github.com>
2019-05-15 02:09:41 -0300

    cambios a la fecha 15/5
    fui creando  el page-item y siguiendo el desarrollo del list - partial

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var requiresLogin= require ('./requireslogin');
 
 /* GET home page. */
 router.get('/',requiresLogin , function(req, res, next) {
-  res.render('index', { title: 'Express',logged:req.session.userId,movies:[] });
+  res.render('index', { title: 'Express',logged:req.session.userId,movies:[],recPerPage:5,currentPage:1,cantPages:5 });
 });
 
 router.get('/login',function(req,res,next){
